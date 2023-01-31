dfd7ea8fa5cca49b247a55974f31d864bdb30156
Reysha28 <irsyalinareysha@gmail.com>
2023-01-31 14:30:29 +0700

    dua

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,18 +70,6 @@
         <h4>Arsip Keuangan</h4>
       </div>
       <div class="card-body">
-        @foreach($commodity_order_by_price as $key => $order_by_price)
-        <ul class="list-unstyled list-unstyled-border">
-          <li class="media">
-            <!-- <img class="mr-3 rounded-circle" width="50" src="../assets/img/avatar/avatar-1.png" alt="avatar"> -->
-            <div class="media-body">
-              <button data-id="{{ $order_by_price->id }}" class="float-right btn btn-info btn-sm show_modal" data-toggle="modal" data-target="#show_commodity">Detail</button>
-              <div class="media-title">{{ $order_by_price->name }}</div>
-              <span class="text-small text-muted">Keterangan{{ $order_by_price->indonesian_currency($order_by_price->price) }}</span>
-            </div>
-          </li>
-        </ul>
-        @endforeach
         <div class="text-center pt-1 pb-1">
           <a href="{{ route('arsip.index') }}" class="btn btn-primary btn-lg btn-round">
             Lihat Semua Arsip
